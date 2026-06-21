cdacc8e83f470d689d8c95aaa52593f45327c302
Sidi Hmety <sidi.ehmety@gmail.com>
2026-06-21 12:58:21 +0000

    fix: enforce idempotency for admin station decisions

--- FuelToken_Backend/addons/acpec_fueltoken_api/tests/__init__.py
+++ FuelToken_Backend/addons/acpec_fueltoken_api/tests/__init__.py
@@ -10,3 +10,4 @@ from . import test_qr_separer_runtime_policy
 from . import test_carnet_transfer_runtime_policy
 from . import test_station_qr_use_runtime_policy
 from . import test_admin_purchase_runtime_policy
+from . import test_admin_station_runtime_policy
