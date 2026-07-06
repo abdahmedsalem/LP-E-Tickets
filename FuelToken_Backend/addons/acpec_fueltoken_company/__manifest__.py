21396b47c051372ea273edf516dc7bf5402f2b8c
Sidi Hmety <sidi.ehmety@gmail.com>
2026-07-06 18:25:31 +0000

    patch43M19 company portal ticket transfer

--- FuelToken_Backend/addons/acpec_fueltoken_company/__manifest__.py
+++ FuelToken_Backend/addons/acpec_fueltoken_company/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'ACPEC FuelToken Company Accounts',
-    'version': '19.0.1.4.4',
+    'version': '19.0.1.4.5',
     'category': 'ACPEC/FuelToken',
     'summary': 'Comptes Sociétés Tickets Carburant pour le back-office ACPEC',
     'author': 'ACPEC SARL',
