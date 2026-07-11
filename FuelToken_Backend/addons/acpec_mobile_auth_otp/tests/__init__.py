ccffe7c83c66122e3e5e06d057a2c49d0f818579
Sidi Hmety <sidi.ehmety@gmail.com>
2026-07-11 12:49:56 +0000

    patch43M23-C3 guard OTP runtime records

--- FuelToken_Backend/addons/acpec_mobile_auth_otp/tests/__init__.py
+++ FuelToken_Backend/addons/acpec_mobile_auth_otp/tests/__init__.py
@@ -2,3 +2,4 @@ from . import test_sms_gateway
 from . import test_res_config_settings_security_table
 from . import test_mobile_pin_reset_otp
 from . import test_mobile_user_blocked_otp
+from . import test_otp_runtime_guards_m23c3
